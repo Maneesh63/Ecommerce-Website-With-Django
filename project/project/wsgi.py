15cd5124c11b27322ddd04f857d2c104fafe4b77
Maneesh63 <maneeshmaneesh391@gmail.com>
2024-09-04 15:07:59 +0530

    mini

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -17,7 +17,7 @@ import sys
 # Add the project directory to the sys.path
 project_home = '/home/Maneesh632/Ecommerce-website-with-django/project/products'
 if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+    sys.path.append(project_home)
 
 # Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
